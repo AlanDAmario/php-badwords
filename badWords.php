373ef06f9468ae4511cdc9cdad640609f614734f
AlanDAmario <vega93@live.it>
2024-06-18 16:52:08 +0200

    [upd] add censored word with all steps

--- badWords.php
+++ badWords.php
@@ -1,1 +1,26 @@
+<?php
 
+echo '<h2>Ecco la parola</h2>';
+echo '<hr>';
+// var_dump($_GET);
+
+//NELLA VARIABILE ANDIAMO A SALVARE IL VALORE ATTRAVERSO IL METODO GET DI 'text', CHE è IL NOME ATTRIBUITO AL TEXT AEREA NELLA PAGNA INDEX.PHP
+$text = $_GET['text'];
+//RICHIAMO VARIABILE PER STAMPA
+echo $text . ' (parola scelta)';
+echo '<hr>';
+// METODO PER AVERE LA LUNGHEZZA DELLA PAROLA 'STRLEN'
+$text_censure = strlen($text);
+
+echo $text_censure . ' lunghezza stringa';
+echo '<hr>';
+$bad_word = $_GET['badword'];
+//METODO PER SOSTITUIRE I VALORI
+$text_clean = str_replace($bad_word, '***', $text);
+echo $bad_word . ' (parola da censurare)';
+echo '<hr>';
+echo $text_clean . ' (parola censurata)';
+
+$text_clean_badword = strlen($text_clean);
+echo '<hr>';
+echo $text_clean_badword . ' (lunghezza parola censurata dopo aver sostituito con i 3 asterischi)';
--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words</title>
 </head>
+
 <body>
     <div>
-        <form action="badWords">
+        <form action="badWords.php" method="get">
             <label for="text">Inserisci parola</label>
             <textarea name="text" id="text"></textarea>
     </div>
-        <hr>
+    <hr>
 
-        <label for="badword">Parola censurata</label>
-        <input type="text" name="badword">
-<hr>
-<button>Invia</button>
+    <label for="badword">Parola censurata</label>
+    <input type="text" name="badword">
+    <hr>
+    <button>Invia</button>
     </form>
 </body>
+
 </html>
